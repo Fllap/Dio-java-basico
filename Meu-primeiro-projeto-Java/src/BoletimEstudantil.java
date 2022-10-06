506b285b44d244bd7b0f525729f733ee65ea7d3b
Felipe <fs0619754@gmail.com>
2022-10-06 10:44:45 -0300

    tipos e variaveis

---
+++ Meu-primeiro-projeto-Java/src/BoletimEstudantil.java
@@ -0,0 +1,14 @@
+public class BoletimEstudantil {
+    public static void main(String[] args) {
+        int mediaFinal = 6;
+            if(mediaFinal < 6){
+                System.out.print("Voce esta de recuperação");
+            }
+            else if(mediaFinal == 6 ){
+                System.out.print("Voce passou raspando");
+            }
+            else{
+                System.out.print("Voce foi Aprovado");
+            }
+    }
+}
